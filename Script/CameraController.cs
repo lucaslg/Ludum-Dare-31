bc4e76886f50e3c213bd1b4d8b215e39a639d0fa
Edridan <axel.deshors@hotmail.fr>
2014-12-06 14:15:06 +0100

    CameraController & sprite city test

---
+++ Script/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float horizontalSpeed = 1f;
+    public float verticalSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float AxisX = Input.GetAxis("Horizontal");
+        float AxisY = Input.GetAxis("Vertical");
+
+        // Update the position
+        transform.Translate(new Vector3(AxisX * horizontalSpeed * Time.deltaTime, AxisY * verticalSpeed * Time.deltaTime, 0));
+	}
+}
