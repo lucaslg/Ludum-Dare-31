cfb163686152f60803249332c7755a061994c02f
lucaslg <lucaslg@users.noreply.github.com>
2014-12-06 12:06:35 +0100

    GameMode and GameState Singletons basic implementation

---
+++ Script/GameMode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMode : MonoBehaviour
+{
+    #region Singleton Implementation
+    private static GameMode _instance = null;
+
+    public static GameMode Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameMode();
+            }
+            return _instance;
+        }
+        set
+        {
+            _instance = value;
+        }
+    }
+    #endregion
+
+    
+}
---
+++ Script/GameState.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour
+{
+    #region Singleton implementation
+    private static GameState _instance = null;
+
+    public static GameState Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameState();
+            }
+            return _instance;
+        }
+        set
+        {
+            _instance = value;
+        }
+    }
+    #endregion
+
+}
