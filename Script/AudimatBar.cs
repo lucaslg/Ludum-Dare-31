539989eabfe85221a87f3b7087d2d5446feb357f
lucaslg <lucaslg@users.noreply.github.com>
2014-12-09 04:14:24 +0100

    Reset error...... fuck the world.....

---
+++ Script/AudimatBar.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudimatBar : MonoBehaviour 
+{
+
+    void Update ()
+    {
+        gameObject.transform.localPosition = - new Vector3(0, 321f - GameState.GetCurrentChannelInstance().CurrentAudimat * 16.05f);
+	}
+
+}
